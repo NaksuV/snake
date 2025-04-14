135d2ec71259661107a941488487b1d96a6aeffc
Naksu <khanahmadimarzieh@gmail.com>
2025-04-15 02:29:25 +0330

    refactor

--- main.js
+++ main.js
@@ -1,54 +1,49 @@
-let direction = "right"
-
-
-
-
-
-
-for (let r = 0; r < 15; r++) {
-    let row = document.createElement("div")
-    row.className = "row"
-
-
-    for (let c = 0; c < 30; c++) {
-        let col = document.createElement("div")
-        col.className = "col"
-        col.id = "r" + r + "c" + c
-        row.append(col)
-    }
-    document.getElementById("board").append(row)
-}
-
-
-
-
+let direction = "down"
 let snake = [[9, 4], [9, 5], [9, 6]]
-for (let s = 0; s < snake.length; s++) {
+let speed = 300
 
-    let target_col = document.getElementById("r" + snake[s][0] + "c" + snake[s][1])
-    target_col.style.backgroundColor = "black"
+creat_board()
+initial_snake_render()
+setInterval(move_snake, speed);
+document.addEventListener("keydown",on_keydown)
 
+function creat_board() {
+    for (let r = 0; r < 15; r++) {
+        let row = document.createElement("div")
+        row.className = "row"
+
+        for (let c = 0; c < 30; c++) {
+            let col = document.createElement("div")
+            col.className = "col"
+            col.id = "r" + r + "c" + c
+            row.append(col)
+        }
+
+        document.getElementById("board").append(row)
+    }
 }
-
-
-
-
-setInterval(() => {
+function initial_snake_render() {
+    for (let s = 0; s < snake.length; s++) {
+        let target_col = document.getElementById("r" + snake[s][0] + "c" + snake[s][1])
+        target_col.style.backgroundColor = "black"
+    }
+}
+function paint_column(col, color) {
+    let col_element = document.getElementById("r" + col[0] + "c" + col[1])
+    col_element.style.backgroundColor = color
+}
+function move_snake() {
     let tail = snake.shift()
-    let tail_col = document.getElementById("r" + tail[0] + "c" + tail[1])
-    tail_col.style.backgroundColor = "white"
+    paint_column(tail, "white")
 
     let head = snake[snake.length - 1]
-
     let new_head
     if (direction === "right") {
         new_head = [head[0], head[1] + 1]
-
     }
     else if (direction === "left") {
         new_head = [head[0], head[1] - 1]
     }
-
     else if (direction === "up") {
         new_head = [head[0] - 1, head[1]]
     }
@@ -57,14 +52,9 @@ setInterval(() => {
     }
 
     snake.push(new_head)
-
-    let head_col = document.getElementById("r" + new_head[0] + "c" + new_head[1])
-    head_col.style.backgroundColor = "black"
-}, 500);
-
-
-
-document.addEventListener("keydown", (event) => {
+    paint_column(new_head, "black")
+}
+function on_keydown(event) {
     if (event.key === "ArrowUp") {
         direction = "up"
     } else if (event.key === "ArrowDown") {
@@ -74,4 +64,4 @@ document.addEventListener("keydown", (event) => {
     } else if (event.key === "ArrowLeft") {
         direction = "left"
     }
-})
+}
