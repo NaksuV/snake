8dc0c55215c7a5627c5cb049ee5a47448fa1cc40
Naksu <khanahmadimarzieh@gmail.com>
2025-04-16 22:28:52 +0330

    implement loop movment

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-let direction = "down"
+let direction = "right"
 let snake = [[9, 4], [9, 5], [9, 6]]
-let speed = 300
+let speed = 500
+let board_width = 30
+let board_height = 15
 
 creat_board()
 initial_snake_render()
 setInterval(move_snake, speed);
-document.addEventListener("keydown",on_keydown)
+document.addEventListener("keydown", on_keydown)
 
 function creat_board() {
-    for (let r = 0; r < 15; r++) {
+    for (let r = 0; r < board_height; r++) {
         let row = document.createElement("div")
         row.className = "row"
 
-        for (let c = 0; c < 30; c++) {
+        for (let c = 0; c < board_width; c++) {
             let col = document.createElement("div")
             col.className = "col"
             col.id = "r" + r + "c" + c
@@ -39,16 +41,32 @@ function move_snake() {
     let head = snake[snake.length - 1]
     let new_head
     if (direction === "right") {
-        new_head = [head[0], head[1] + 1]
+        if (head[1] === board_width - 1) {
+            new_head = [head[0], 0]
+        } else {
+            new_head = [head[0], head[1] + 1]
+        }
     }
     else if (direction === "left") {
-        new_head = [head[0], head[1] - 1]
+        if (head[1] === 0) {
+            new_head = [head[0], board_width - 1]
+        } else {
+            new_head = [head[0], head[1] - 1]
+        }
     }
     else if (direction === "up") {
+        if (head[0] === 0) {
+            new_head = [board_height - 1, head[1]]    
+        }else{
         new_head = [head[0] - 1, head[1]]
+        }
     }
     else if (direction === "down") {
+        if (head [0] === board_height - 1) {
+            new_head = [0 , head[1]]
+        }else{
         new_head = [head[0] + 1, head[1]]
+        }
     }
 
     snake.push(new_head)
